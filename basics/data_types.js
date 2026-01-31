4328b7d52d7446bc507fe253a90a5b9ff4b2b067
Madhav Dang <madhav3971@gmail.com>
2026-01-31 20:30:28 +0000

    basics

---
+++ basics/data_types.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // using node js
+
+console.log(4+4)
+ console.log("hello")
+
+ let name = "madhav"
+ let age = 22
+ let isloggedin = false
+ let city; /*undefined */
+
+ //number=>2to pwer 53
+ //bigint
+ //string
+//boolean
+//null
+//symbol=>uniqueness
+
+//object
+
+console.log(typeof 2);
+console.log(typeof null);
+
---
+++ basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 123456
+let accountEmail = "ninvijn"
+var accountPassword = "123121231"
+accountCity = " Bulandshar"
+let accountState
+
+//  accountId = 45453 //
+
+accountEmail="hbcibcis"
+accountPassword="353535"
+accountCity = " ivi"
+
+console.log(accountId);
+
+
+/* 
+PREFER not to use var.
+*/
+
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
