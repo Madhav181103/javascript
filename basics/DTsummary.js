eebd5c0eddad1bb6a8e5cf0a47b69d673eee1b63
Madhav Dang <madhav3971@gmail.com>
2026-02-01 16:43:28 +0000

    summary of datatypes

---
+++ basics/DTsummary.js
@@ -0,0 +1,20 @@
+// Primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorescore = 100.01
+
+const isloggedin = false
+const outsidetemp = null
+let useremail;
+const id =Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(anotherid===id);
+
+
+
+//refernce (non primitive)
+
+//Array,Objects,Functions
