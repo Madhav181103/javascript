cdbf2f9251811df73abd2afbfb2e7f761f0e2884
Madhav Dang <madhav3971@gmail.com>
2026-02-01 16:02:34 +0000

    conversion

---
+++ basics/conversions.js
@@ -0,0 +1,9 @@
+let isloggedin 
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(typeof(stringnumber))
