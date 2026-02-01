99b9197fc98e2763bbff77a9356714ebed72928c
Madhav Dang <madhav3971@gmail.com>
2026-02-01 16:32:16 +0000

    basics 2

--- basics/conversions.js
+++ basics/conversions.js
@@ -7,3 +7,18 @@ let somenumber = 33
 
 let stringnumber = String(somenumber)
 console.log(typeof(stringnumber))
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2**3);
+
+
+
+
+
+
