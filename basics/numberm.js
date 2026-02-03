3873a70891743cef0ffa2c52712e5f7a2e3f609a
Madhav Dang <madhav3971@gmail.com>
2026-02-03 15:49:50 +0000

    number and maths

---
+++ basics/numberm.js
@@ -0,0 +1,36 @@
+const score =400
+
+const balance = new Number(90)
+// console.log(balance);
+// console.log(score);
+
+// console.log(typeof(balance.toString()))
+
+const othernumber=123.89911
+// console.log(othernumber.toPrecision(5));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//this was all about numbers
+
+//++++++++MATHS++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.001));
+//.min & .max
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const max=100
+const min=0
+
+console.log(Math.floor(Math.random()* (max-min+1))+min);
+
+
+
+
